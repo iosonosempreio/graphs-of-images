932f3397debc2afec28feffe78b292d3e0adc8df
iosonosempreio <iosonosempreio@users.noreply.github.com>
2021-01-22 23:50:08 +0100

    Update Network.js

--- src/components/network/Network.js
+++ src/components/network/Network.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useRef } from "react";
 import * as PIXI from "pixi.js";
-import { json } from "d3";
+import { json, scaleLinear, extent } from "d3";
 import { Viewport } from "pixi-viewport";
 
-const width = window.innerWidth, height = window.innerHeight;
-let data, nodes, edges;
+const width = window.innerWidth,
+  height = window.innerHeight,
+  size = scaleLinear();
+let nodes, hashtags, missings;
 
 let app = new PIXI.Application({
   backgroundColor: 0xf1f1f1,
   resolution: window.devicePixelRatio || 1,
   width: width,
-  height: height
+  height: height,
+  antialias: true
 });
 
 const viewport = new Viewport({
@@ -20,13 +23,9 @@ const viewport = new Viewport({
   screenHeight: height,
   worldWidth: width,
   worldHeight: height,
-  interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+  interaction: app.renderer.plugins.interaction, // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
 });
-viewport
-  // .clampZoom(Utilities.clampZoomOptions)
-  .drag({pressDrag:true, clampWheel:true})
-  .pinch()
-  .wheel()
+viewport.drag({ pressDrag: true, clampWheel: true }).pinch().wheel();
 app.stage.addChild(viewport);
 
 const container = new PIXI.Container();
@@ -40,7 +39,10 @@ container.y = app.screen.height / 2;
 container.pivot.x = container.width / 2;
 container.pivot.y = container.height / 2;
 
-const spritesheetsList = Array.from( Array(15), (x,i)=>`images/spritesheets/spritesheet-${i+1}.json`)
+const spritesheetsList = Array.from(
+  Array(15),
+  (x, i) => `images/spritesheets/spritesheet-${i + 1}.json`
+);
 
 spritesheetsList.forEach((d) => {
   app.loader.add(process.env.PUBLIC_URL + d);
@@ -48,8 +50,62 @@ spritesheetsList.forEach((d) => {
 app.loader.onProgress.add((e) => {
   console.log(e.progress + "%");
 });
+
 app.loader.onComplete.add(async () => {
   console.log("loader completed", app.loader.resources);
+
+  const graphics = new PIXI.Graphics();
+  missings.forEach(m=>{
+    const my_size = size(m.size);
+    graphics.lineStyle(1, 0x000000, 1);
+    // graphics.beginFill(0xDE3249);
+    graphics.drawRect(m.x - my_size/2, m.y - my_size/2, my_size, my_size);
+    const l1_x1 = m.x - my_size/2,
+          l1_y1 = m.y - my_size/2,
+          l1_x2 = m.x + my_size/2,
+          l1_y2 = m.y + my_size/2;
+    graphics.moveTo(l1_x1, l1_y1);
+    graphics.lineTo(l1_x2, l1_y2);
+    const l2_x1 = m.x + my_size/2,
+          l2_y1 = m.y - my_size/2,
+          l2_x2 = m.x - my_size/2,
+          l2_y2 = m.y + my_size/2;
+    graphics.moveTo(l2_x1, l2_y1);
+    graphics.lineTo(l2_x2, l2_y2);
+    graphics.closePath();
+    // graphics.endFill();
+  })
+  container.addChild(graphics);
+
+  hashtags.forEach((h) => {
+    let style = new PIXI.TextStyle({
+      "fontSize": h.size,
+      "fontFamily": "Arial",
+      // fontStyle: "italic",
+      // fontWeight: "normal",
+      fill:"#333333"
+    });
+    style = h.size > 50 ? {
+      ...style,
+      fill: ["#ffffff", "#00ff99"], // gradient
+      stroke: "#4a1850",
+      strokeThickness: 5,
+      // dropShadow: true,
+      // dropShadowColor: "#000000",
+      // dropShadowBlur: 4,
+      // dropShadowAngle: Math.PI / 6,
+      // dropShadowDistance: 6,
+      lineJoin: "round",
+    } : style;
+    const text = new PIXI.Text(h.label, style);
+    text.x = h.x;
+    text.y = h.y*-1;
+    text.anchor.x = 0.5;
+    text.alpha = h.size > 50 ? 1 : 0.5;
+    // text.anchor.y = 0.5;
+    container.addChild(text);
+  });
+
   spritesheetsList.forEach((s) => {
     const sheet = app.loader.resources[s].spritesheet;
     for (let textureName in sheet.textures) {
@@ -57,20 +113,26 @@ app.loader.onComplete.add(async () => {
       const texture = sheet.textures[textureName];
       const sprite = new PIXI.Sprite(texture);
 
-      const nodeData = nodes.find(n=>n.attributes.image===textureName)
-
+      const nodeData = nodes.find((n) => n.attributes.image === textureName);
+      let scale = 1;
       if (nodeData) {
-        sprite.x = nodeData.x *0.5
-        sprite.y = nodeData.y *0.5
+        sprite.x = nodeData.x;
+        sprite.y = nodeData.y*-1;
+        scale = Math.min(
+          size(nodeData.size) / texture.orig.width,
+          size(nodeData.size) / texture.orig.height
+        );
       } else {
-        sprite.x = 0
-        sprite.y = 0
+        sprite.x = 0;
+        sprite.y = 0;
+        scale = Math.min(
+          50 / texture.orig.width,
+          50 / texture.orig.height
+        );
       }
 
       sprite.anchor.x = 0.5;
       sprite.anchor.y = 0.5;
-
-      const scale = Math.min(50 / texture.orig.width, 100 / texture.orig.height);
       sprite.scale.x = scale;
       sprite.scale.y = scale;
 
@@ -82,11 +144,15 @@ app.loader.onComplete.add(async () => {
 function Network() {
   const container = useRef();
   useEffect(() => {
-    json(process.env.PUBLIC_URL + "data.json").then(data=>{
-      nodes = data.nodes.filter(n=>n.attributes.type==="post")
+    json(process.env.PUBLIC_URL + "data.json").then((data) => {
+      nodes = data.nodes.filter((n) => n.attributes.type === "post");
+      hashtags = data.nodes.filter((n) => n.attributes.type === "hashtag");
+      missings = data.nodes.filter((n) => n.attributes.type === "postNoImage");
+      console.log(missings);
+      size.domain(extent(nodes, d=>d.size)).range([35,200])
       container.current.appendChild(app.view);
       app.loader.load();
-    })
+    });
   }, []);
 
   return <div ref={container}></div>;
